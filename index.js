a53c39bfdfe3b123876fa8fd9e376fc496073511
Christopher Jackson <Chris2003jackson@gmail.com>
2022-01-18 08:47:58 -0500

    push

---
+++ index.js
@@ -0,0 +1,22 @@
+// the first alert
+alert("Hello World");
+
+// the second alert
+alert("Hello World");
+
+var myVariable1;
+myVariable1 = 10;
+
+var myVar2 = "hello";
+
+myVar2 = 5;
+
+var customerAge = 32;
+
+var myVar3 = 40;
+
+var myVar = 5;
+
+var myVar4 =10;
+
+ 
